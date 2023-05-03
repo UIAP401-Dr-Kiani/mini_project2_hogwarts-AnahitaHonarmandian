c158363b6572015596e1937718177a8fbe6bb905
Ana <ana.honarmandian@gmail.com>
2023-05-03 21:52:50 +0430

    human properties

--- HarryPotter/Human.cs
+++ HarryPotter/Human.cs
@@ -6,17 +6,48 @@ using System.Threading.Tasks;
 
 namespace HarryPotter
 {
-     class Human
+    class Human
     {
         private string _firstName;
         private string _lastName;
         private uint _BirthYear;
         private Boolean _sex;
         private Human _Father;
-        private string username;
+        private readonly string _username;
         private string password;
         private RaceBlood race;
-        
+
+        public string FirstName
+        {
+            get { return _firstName; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+        }
+        public uint BirthYear
+        {
+            get { return _BirthYear; }
+        }
+        public Boolean Sex
+        {
+            get { return _sex; }
+        }
+        public Human Father
+        {
+            get { return _Father; }
+        }
+        public string Username
+        {
+            get { return _username; }
+        }
+        public string Password
+        {
+            get { return password; }
+        }
+        public RaceBlood Race
+        {
+            get { return race; }
+        }
     }
-    
 }
