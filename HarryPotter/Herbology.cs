73429341227793223b2a29d6a9bf59520b4b2e72
Ana <ana.honarmandian@gmail.com>
2023-05-08 14:33:25 +0430

    Completed herbology class constructor and properties

--- HarryPotter/Herbology.cs
+++ HarryPotter/Herbology.cs
@@ -7,9 +7,25 @@ using System.Threading.Tasks;
 namespace HarryPotter
 {
      class Herbology : Subjects
-    //study of magical plants
+    
     {
-        //TODO - fields and properties
+        
+        public List<Semester1> Semester1Plants { get;  }
+        public List<Semester2> Semester2Plants { get; }
+        public List<Semester3> Semester3Plants { get; }
+
+        public Herbology(string name, int capacity, uint semesterPresentation, uint minutesDuration):
+            base( name, capacity, semesterPresentation, minutesDuration)
+        {
+            Semester1Plants = new List<Semester1>();
+            Semester2Plants = new List<Semester2>();
+            Semester3Plants = new List<Semester3>();
+
+
+        }
+
+           
+
 
     }
 }
