c6f453aabfe049e3411a510904cb19f95853aeb3
Ana <ana.honarmandian@gmail.com>
2023-05-08 12:17:59 +0430

    dumbledore class constructor

--- HarryPotter/Dumbledore.cs
+++ HarryPotter/Dumbledore.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static HarryPotter.Enums;
 
 namespace HarryPotter
 {
@@ -11,10 +12,18 @@ namespace HarryPotter
         
         public List<Dorms> DormList { get ; }
 
-       public Dumbledore() {
-        
+        public Dumbledore(List<Dorms> dormList):
+             base("Albus", "Dumbledore", 1800,
+            Enums.Gender.MALE, "Christopher",
+            "Admin", "1", Enums.RaceBlood.PURE_BLOOD,
+            0, Enums.Pet.OWL, true, Enums.Role.TEACHER)
+        {
+            DormList = dormList;
         }
 
 
-    }
+        }
+
+
+    
 }
